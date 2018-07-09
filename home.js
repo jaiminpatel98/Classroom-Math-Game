1f6dfe5729e9a63a7f7836926c636dbde46a3bc9
Jaimin Patel <jaiminpatel98@gmail.com>
2018-07-09 16:04:26 -0400

    added hover buttons, buttons, footers

---
+++ home.js
@@ -0,0 +1,31 @@
+function playHover(element) {
+    element.setAttribute('src', 'images/play-icon-h.png');
+}
+
+function playUnhover(element) {
+    element.setAttribute('src', 'images/play-icon.png');
+}
+
+function questionHover(element) {
+    element.setAttribute('src', 'images/question-icon-h.png');
+}
+
+function questionUnhover(element) {
+    element.setAttribute('src', 'images/question-icon.png');
+}
+
+function settingsHover(element) {
+    element.setAttribute('src', 'images/settings-icon-h.png');
+}
+
+function settingsUnhover(element) {
+    element.setAttribute('src', 'images/settings-icon.png');
+}
+
+function historyHover(element) {
+    element.setAttribute('src', 'images/history-icon-h.png');
+}
+
+function historyUnhover(element) {
+    element.setAttribute('src', 'images/history-icon.png');
+}
