6608a7239e8181b3b7979e01ed86cd2f11e2cc03
SamWorrod <samworrod@gmail.com>
2018-07-11 22:17:51 -0500

    added keypress functionality to additiongameplay page

--- additiongameplay.js
+++ additiongameplay.js
@@ -1,16 +1,54 @@
-function leftHover(element) {
+function arrowPressed(event) {
+    var keyPressed = event.charCode || event.keyCode;
+    var getKey = String.fromCharCode(keyPressed);
+    switch(getKey) {
+        case "%":
+            document.getElementById("left").setAttribute('src', 'images/arrow-left-h.png');
+            break;
+        case "'":
+            document.getElementById("right").setAttribute('src', 'images/arrow-right-h.png');
+            break;
+        case "&":
+            document.getElementById("up").setAttribute('src', 'images/arrow-up-h.png');
+            break;
+        case "(":
+            document.getElementById("down").setAttribute('src', 'images/arrow-down-h.png');
+    }
+
+}
+
+function arrowPressedDone(event) {
+    var keyPressed = event.charCode || event.keyCode;
+    var getKey = String.fromCharCode(keyPressed);
+    switch(getKey) {
+        case "%":
+            document.getElementById("left").setAttribute('src', 'images/arrow-left.png');
+            break;
+        case "'":
+            document.getElementById("right").setAttribute('src', 'images/arrow-right.png');
+            break;
+        case "&":
+            document.getElementById("up").setAttribute('src', 'images/arrow-up.png');
+            break;
+        case "(":
+            document.getElementById("down").setAttribute('src', 'images/arrow-down.png');
+    }
+
+}
+
+function leftKeyDown(element) {
     element.setAttribute('src', 'images/arrow-left-h.png');
 }
 
-function leftUnhover(element) {
+function leftKeyUp(element) {
     element.setAttribute('src', 'images/arrow-left.png');
 }
 
-function rightHover(element) {
+function rightKeyDown(element) {
     element.setAttribute('src', 'images/arrow-right-h.png');
 }
 
-function rightUnhover(element) {
+function rightKeyUp(element) {
     element.setAttribute('src', 'images/arrow-right.png');
 }
 
