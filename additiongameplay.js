e1ad91b003f12ba2e193c31bce753256120fb655
SamWorrod <samworrod@gmail.com>
2018-07-11 21:47:12 -0500

    Added addition gameplay page + references

---
+++ additiongameplay.js
@@ -0,0 +1,31 @@
+function leftHover(element) {
+    element.setAttribute('src', 'images/arrow-left-h.png');
+}
+
+function leftUnhover(element) {
+    element.setAttribute('src', 'images/arrow-left.png');
+}
+
+function rightHover(element) {
+    element.setAttribute('src', 'images/arrow-right-h.png');
+}
+
+function rightUnhover(element) {
+    element.setAttribute('src', 'images/arrow-right.png');
+}
+
+function upHover(element) {
+    element.setAttribute('src', 'images/arrow-up-h.png');
+}
+
+function upUnhover(element) {
+    element.setAttribute('src', 'images/arrow-up.png');
+}
+
+function downHover(element) {
+    element.setAttribute('src', 'images/arrow-down-h.png');
+}
+
+function downUnhover(element) {
+    element.setAttribute('src', 'images/arrow-down.png');
+}
